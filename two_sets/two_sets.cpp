b01a2aec94f7c6e19301bd384d43d6dbdc5fb0e8
Aaron Hirvi <aaron.hirvi@tuni.fi>
2022-04-04 20:21:53 +0300

    added missing task

--- two_sets/two_sets.cpp
+++ two_sets/two_sets.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <set>
 
-int main()
-{
+int main(){
     long int input;
 
     std::deque<long int> Q1;
@@ -13,13 +13,38 @@ int main()
 
     std::cin >> input;
 
-    long int sum = input * (input + 1) / 2;
+    long int sum = input *(input+1)/2;
 
-    if (sum % 2 == 1)
-    {
-        std::cout << "NO" << std::endl;
-        return;
+    if(sum % 2 == 1){
+        std::cout<< "NO" << std::endl;
+        return 0;
     }
 
-    return 1;
+    long int halfway = sum /2;
+
+    for(long int i = input; i >= 1; i--){
+
+        if(i <= halfway){
+            Q1.push_back(i);
+            halfway -= i;
+        }else{
+            Q2.push_back(i);
+        }
+    }
+
+    std::cout << "YES" << std::endl;
+
+    std::cout << Q1.size() << std::endl;
+    for(int x : Q1){
+        std::cout << x << " ";
+    }
+
+    std::cout << std::endl;
+
+    std::cout << Q2.size() << std::endl;
+    for(int y : Q2){
+        std::cout << y << " ";
+    }
+
+    return 0;
 }
